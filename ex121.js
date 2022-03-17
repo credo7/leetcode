f597c8e6b557083ede59fe83106b6f0f9187dc19
credo7 <ah.vit@mail.ru>
2022-03-17 19:57:17 +0300

    ex121 completed

---
+++ ex121.js
@@ -0,0 +1,15 @@
+var maxProfit = function (prices) {
+  let max = 0;
+  let l = 0;
+  let r = 1;
+  while (r < prices.length) {
+    if (prices[l] < prices[r]) {
+      let profit = prices[r] - prices[l];
+      if (profit > max) max = profit;
+    } else {
+      l = r;
+    }
+    r++;
+  }
+  return max;
+};
